d211afca0fe7ab501224e6bd46ee205c9102f5ab
Stefanos Pavlidis <swp1999@hotmail.gr>
2021-02-06 01:25:25 +0200

    RulesPage

---
+++ lib/rules.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class RulesPage extends StatelessWidget {
+  static String title = "Rules";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    text: 'Step 1',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text:
+                            ': Players split up into 2 teams of equal members and they press play choosing the amount of rounds',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: 'Step 2',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text:
+                            ': The teams take turns to play. The team that plays first selects a member. This member will have the device and will be given a random generated category and will be asked to enter 5 words of that category that come to his mind',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: 'Step 3',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text:
+                            ": The rest of the team have 1 minute to guess those 5 words, while knowing the category they are playing in, without the help of the player that entered the words. When a word is found, the player taps on the word and it awards the team 1 point. When time is up, the other team gets the device and repeat the process. When that team's time is up that concludes round 1 and it is the first team's time to play but they cannot choose the same player to enter the words. ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: 'Winner is ',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text:
+                            ': The team that has accumulated more points at the end of the last round',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
