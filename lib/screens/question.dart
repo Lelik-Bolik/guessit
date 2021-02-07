9d92bfba565c227647f2cd6839c796e999896394
Alex Vavvas <a.d.vavvas@gmail.com>
2021-02-08 00:12:23 +0200

    Clean up, code refactor, and commented QuestionPage related widgets

--- lib/screens/question.dart
+++ lib/screens/question.dart
@@ -1,62 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:guessit/widgets/option.dart';
+import 'package:guessit/widgets/options_list.dart';
 import 'package:guessit/widgets/question_container.dart';
 import 'package:guessit/widgets/question_header.dart';
 
+/// The screen/page that is displayed during gameplay.
+///
+/// This page is displayed after the user has selected the answers to the question,
+/// and is used to select the valid answers of the team-members (as well as show the question).
 class QuestionPage extends StatelessWidget {
-  final String question;
-  final List<String> answers;
-
   QuestionPage({this.question, this.answers});
+
+  /// The question that currently playing team must answer.
+  final String question;
+
+  /// A list of the proposed answers.
+  final List<String> answers;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        // Make bottom true and put positioned container to cover the gap (in the black background)
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Column(
-                  children: [
-                    Flexible(
-                        fit: FlexFit.loose,
-                        flex: 1,
-                        child: QuestionHeader()),
-                    Flexible(
-                        fit: FlexFit.loose,
-                        flex: 6,
-                        child: QuestionContainer()),
-                  ],
-                ),
-              ),
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.black,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          SizedBox(height: 32),
-                          Option(),
-                          Option(),
-                          Option(),
-                          Option(),
-                          SizedBox(height: 32),
-                        ],
-                      ),
-                    ),
-                  )),
-            ],
+      // Stack that contains a `Container` and a `SafeArea`.
+      // The Container fills the SafeArea at the bottom of the screen.
+      // with background color (black for light theme, white for dark theme).
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).padding.bottom,
+              color: Colors.black,
+            ),
           ),
-        ),
+          SafeArea(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        Flexible(
+                            fit: FlexFit.loose,
+                            flex: 1,
+                            child: QuestionHeader()),
+                        Flexible(
+                            fit: FlexFit.loose,
+                            flex: 6,
+                            child: QuestionContainer()),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.black,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: OptionsList(
+                            // TODO: Set `options` programmatically
+                            options: [
+                              "Option 1",
+                              "Selection 2",
+                              "Choice 3",
+                              "Option 4",
+                              "Choice 3",
+                            ],
+                          ),
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/options_list.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:guessit/widgets/option.dart';
+
+/// The widget that holds the [Option] widgets.
+///
+/// A `Column` with [Option] wrapped in `Expanded`.
+class OptionsList extends StatefulWidget {
+  const OptionsList({
+    Key key,
+    @required this.options,
+  }) : super(key: key);
+
+  /// A List of proposed options (answers) for the question.
+  ///
+  /// Each option is represented by a `String`.
+  final List<String> options;
+
+  @override
+  State<StatefulWidget> createState() => OptionListState();
+}
+
+/// The `State` for the [OptionsList]
+class OptionListState extends State<OptionsList> {
+  /// The options that have been selected.
+  ///
+  /// A `List` of `int`s representing the indexes of the selected options.
+  List<int> _selectedItems = List<int>();
+  // TODO: Implement the selection process.
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        // Returns a List of pairs of Widgets, a `SizedBox` and the [Option].
+        // If the generated Widgets are the last in the list, we generate a trailing `SizedBox`.
+        // We `expand` the result of the `generate` so that it returns a single flat (not nested) List.
+        ...List.generate(widget.options.length, (index) {
+          return [
+            SizedBox(height: 16),
+            Expanded(
+                child: Option(
+              index: index,
+              text: widget.options[index],
+              // TODO: Set 'onTap'
+              onTap: () => print("Tap"),
+            )),
+            if (index == widget.options.length - 1) SizedBox(height: 16)
+          ];
+        }).expand((element) => element).toList()
+      ],
+    );
+  }
+}
